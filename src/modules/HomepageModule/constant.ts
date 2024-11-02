bee31100ee78a7eee17b84097a58b16efa31e8e6
gracekarinn <gracekarina12@gmail.com>
2024-11-03 03:13:00 +0700

    feat: add constant file for homepage data

---
+++ src/modules/HomepageModule/constant.ts
@@ -0,0 +1,17 @@
+export const HOMEPAGE_DATA = [
+  {
+    title: "Penyimpanan Data Pribadi yang Lebih Aman",
+    description:
+      "Data kesehatan Anda dikirim dengan aman menggunakan teknologi blockchain, menjamin privasi dan integritas.",
+  },
+  {
+    title: "Riwayat Medis Terjamin",
+    description:
+      "JagaJKN menyimpan seluruh riwayat medis Anda secara aman di blockchain, sehingga data tidak dapat dimodifikasi atau diakses oleh pihak yang tidak berwenang. ",
+  },
+  {
+    title: "Kontrol Penggunaan Data Pribadi Anda",
+    description:
+      "JagaJKN memberi Anda kontrol penuh atas data pribadi Anda. Hanya Anda dan pihak medis yang berwenang yang dapat mengakses informasi sensitif, memberikan privasi penuh.",
+  },
+];
