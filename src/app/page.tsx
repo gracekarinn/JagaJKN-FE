3c039ddf63cc13d2b8b952c05e40de0efdf5b34c
gracekarinn <gracekarina12@gmail.com>
2024-11-03 05:26:55 +0700

    feat: add Navbar and Footer to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 import { HomepageModule } from "@/modules/HomepageModule";
 import Image from "next/image";
+import { Navbar } from "./components/navbar";
+import Footer from "./components/footer";
 
 export default function Home() {
-  return <HomepageModule />;
+  return (
+    <main>
+      <Navbar />
+      <HomepageModule />
+      <Footer />
+    </main>
+  );
 }
