0bb961b2ae9e0e8f107a1b5b918b193b2acdd216
gracekarinn <gracekarina12@gmail.com>
2024-11-03 16:15:36 +0700

    feat: update footer position to be fixed at the bottom

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export const Footer = () => {
   return (
-    <footer className="flex flex-col mx-8 gap-y-8 py-12">
+    <footer className="bottom-0 flex flex-col mx-8 gap-y-8 py-12">
       <div className="flex items-center gap-x-4">
         <div className="relative w-[200px] h-[72px]">
           <Image
