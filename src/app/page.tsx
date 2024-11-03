9a810bac0b679e733d7e1c1a3df0775c2513824e
gracekarinn <gracekarina12@gmail.com>
2024-11-03 17:17:09 +0700

    refactor: remove unused next/image import in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { HomepageModule } from "@/modules/HomepageModule";
-import Image from "next/image";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
 
